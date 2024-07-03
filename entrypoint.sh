17e4959811c290b7046da6eb999c675cb7fcccb7
lightlike <dnietfeld@outlook.com>
2024-07-03 17:27:15 +0200

    run ansible once on startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,8 @@
 echo "Creating cronjob with schedule: ${SCHEDULE}"
 echo "${SCHEDULE} ansible-playbook /netcup-dns/ansible/update-dns.yaml" > /etc/crontabs/root
 
+echo "Running task once"
+exec "ansible-playbook /netcup-dns/ansible/update-dns.yaml"
+
 echo "Running command: '$@'"
 exec "$@"
